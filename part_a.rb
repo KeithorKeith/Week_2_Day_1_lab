16cad8cf6da9567b0bfaf60a0325c1989013709d
KeithorKeith <keithnimmo@hotmail.co.uk>
2020-05-18 18:59:57 +0100

    completed section A

--- part_a.rb
+++ part_a.rb
@@ -4,10 +4,29 @@ class Student
         @student_name = student_name
         @cohort = cohort
     end
-
+    #getters
     def student_name
         return @student_name
     end
 
+    def cohort
+        return @cohort
+    end
+    #setters
+    def set_student_name(student_name)
+        @student_name = student_name
+    end
+
+    def set_cohort(cohort)
+        @cohort = cohort
+    end
+    #talking
+    def talking(speach)
+        return "#{speach}"
+    end
+
+    def say_favourite_language(language)
+        return "I love #{language}"
+    end
 
 end
--- specs/part_a_spec.rb
+++ specs/part_a_spec.rb
@@ -1,12 +1,40 @@
 require('minitest/autorun')
 require('minitest/reporters')
-require_relative('../bank_account')
+require_relative('../part_a')
+
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestStudent < MiniTest::Test
+
   def test_student_name
     student_account = Student.new("Keith", "e40")
     assert_equal("Keith", student_account.student_name())
   end
 
+  def test_cohort
+    student_account = Student.new("Keith", "e40")
+    assert_equal("e40", student_account.cohort())
+  end
+
+  def test_update_student_name
+    student_account = Student.new("Keith", "e40")
+    student_account.set_student_name("Kraig")
+    assert_equal("Kraig", student_account.student_name())
+  end
+
+  def test_update_student_name
+    student_account = Student.new("Keith", "e40")
+    student_account.set_cohort("e30")
+    assert_equal("e30", student_account.cohort())
+  end
+
+  def test_talking
+    student_account = Student.new("Keith", "e40")
+    assert_equal("I can talk!", student_account.talking("I can talk!"))
+  end
+
+  def test_say_favourite_language
+    student_account = Student.new("Keith", "e40")
+    assert_equal("I love ruby", student_account.say_favourite_language("ruby"))
+  end
 end
